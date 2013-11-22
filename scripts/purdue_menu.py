ee4b2d4f4794a3853cae28e5d875f46a3eb26ef9
Evidlo <evan@evanw.org>
2013-11-22 11:53:39 -0500

    Fixed some bugs in some things, and other stuff.

--- scripts/purdue_menu.py
+++ scripts/purdue_menu.py
@@ -11,7 +11,7 @@ def getItems(court_meal,bar_in): #Grab menu items provided [dining
     items=[]
     for bar in court_meal.getchildren():
         if bar.Name == bar_in:
-            for item in menu.Items.getchildren():
+            for item in bar.Items.getchildren():
                 items.append(item.Name)
     return items
 
@@ -20,6 +20,7 @@ def diningHelp():
     dining_courts=["earhart","ford","hillenbrand","wiley","windsor"]
     print "Usage: purdue_menu COURT BAR1 [BAR2] ..."
     print "To get a list of bars: purdue_menu COURT -h\n"
+    print "Example usage: purdue_menu hillenbrand \"Granite Grill\""
     print "Available dining courts: ",
     for dining_court in dining_courts:
         print dining_court+", ",
@@ -41,7 +42,7 @@ url="http://api.hfs.purdue.edu/menus/v1/locations/"+dining_court+"/"+time.strfti
 urllib.urlopen(url)
 dining_court_obj = objectify.fromstring(urllib.urlopen(url).read())
 
-if len(sys.argv) < 3 or sys_argv[2] == "-h":
+if len(sys.argv) < 3 or sys.argv[2] == "-h":
     barHelp(dining_court_obj)
     
 bars = sys.argv[2:len(sys.argv)]    
@@ -63,7 +64,7 @@ for bar in bars:
  
 
 for menu in menus:
-    for item in menu[:3]:
+    for item in menu:
         print item+", ",
 
 

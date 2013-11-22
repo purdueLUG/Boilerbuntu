138c1a192c1de3115c98a3034c6d48f14fb95060
Nick Geirland <ngeirlan@purdue.edu>
2013-11-22 11:50:15 -0500

    sys.argv not sys_argv

--- scripts/purdue_menu.py
+++ scripts/purdue_menu.py
@@ -41,7 +41,7 @@ url="http://api.hfs.purdue.edu/menus/v1/locations/"+dining_court+"/"+time.strfti
 urllib.urlopen(url)
 dining_court_obj = objectify.fromstring(urllib.urlopen(url).read())
 
-if len(sys.argv) < 3 or sys_argv[2] == "-h":
+if len(sys.argv) < 3 or sys.argv[2] == "-h":
     barHelp(dining_court_obj)
     
 bars = sys.argv[2:len(sys.argv)]    
